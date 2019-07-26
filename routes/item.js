655e1f0ef258f076fd4ae7295112bb08f285dbe8
Monika-Mistry <monika.mistry1@gmail.com>
2019-07-26 11:23:01 +0100

    post method added

--- routes/item.js
+++ routes/item.js
@@ -4,16 +4,27 @@ const router = express.Router();
 
 const Item = require("../models/itemModel");
 
+const success = { success: true };
 // @route POST item/addItem
 // @desc Add an item
 // @access Public
+router.post("/addItem", (req, res) => {
+    const newItem = new Item({
+        username: req.body.username,
+        content: req.body.content
+    });
+
+    newItem.save().then(() => {
+        res.send(success);
+    }).catch(err => res.status(404).send(err));
+});
 
 // @route GET item/getItems
 // @desc Get items with specified username
 // @access Public
 router.get("/getItems", (req, res) => {
     const errors = {};
-    Item.find(req.body).then(items => {
+    Item.find(req.body, '-__v').then(items => {
         if (!items || items.length < 1) {
             errors.noItems = "There are no items with this username: " + req.body.username;
             res.status(404).send(errors);
