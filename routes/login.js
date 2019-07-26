1f2143f1df1ef5c96597f844b03f0996e0951d78
Monika-Mistry <monika.mistry1@gmail.com>
2019-07-26 10:02:36 +0100

    basic updatelogin method

--- routes/login.js
+++ routes/login.js
@@ -47,9 +47,25 @@ router.get("/getUser", (req, res) => {
 // @route PUT login/updateUser
 // @desc Update a users' details
 // @access Public
-// router.put("/updateUser", (req, res) => {
+router.put("/updateUser", (req, res) => {
+    const errors = {};
 
-// });
+    const search = { email: req.body.email };
+    const update = { username: req.body.username };
+
+    Login.updateOne(search, update)
+        .then(user => {
+            if (user.n === 1 && user.nModified === 1) {
+                res.send(success);
+            }
+            else {
+                errors.noUpdate = "The user has not been updated";
+                res.status(404).json(errors);
+            }
+        })
+        .catch(err => res.status(404).send(err));
+
+});
 
 // @route DELETE login/deleteUser
 // @desc Remove a user with a given email
@@ -57,15 +73,15 @@ router.get("/getUser", (req, res) => {
 router.delete("/deleteUser", (req, res) => {
     const errors = {};
     Login.deleteOne(req.body)
-    .then(user => {
-        if (user.ok === 1) {
-            res.send(success);
-        } else {
-            errors.notDeleted = "The user has not been deleted";
-            res.status(404).json(errors);
-        }
-    })
-    .catch(err => res.status(404).send(err));
+        .then(user => {
+            if (user.ok === 1) {
+                res.send(success);
+            } else {
+                errors.noDelete = "The user has not been deleted";
+                res.status(404).json(errors);
+            }
+        })
+        .catch(err => res.status(404).send(err));
 
 });
 
